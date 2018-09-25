2ef6e9df07b7720ce48c5fc6f5906818959fcba1
João Manoel Neto <joaaomanooel@gmail.com>
2018-09-25 13:45:20 -0300

    add jwt auth

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import config from './config/config';
 import datasource from './config/datasource';
 import booksRouter from './routes/books';
 import usersRouter from './routes/users';
+import authRouter from './routes/auth';
+import authorization from './auth';
 
 const app = express();
 app.config = config;
@@ -11,7 +13,12 @@ app.datasource = datasource(app);
 app.set('port', 7000);
 app.use(bodyParser.json());
 
+const auth = authorization(app);
+app.use(auth.initialize());
+app.auth = auth;
+
 booksRouter(app);
 usersRouter(app);
+authRouter(app);
 
 export default app;
