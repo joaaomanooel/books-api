8e885b2c84b7b0abb08eb9a222e78dcda8e14b72
João Manoel Neto <joaaomanooel@gmail.com>
2018-10-22 14:04:53 -0300

    change port

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.set('port', 7000);
 const auth = authorization(app);
 app.use(auth.initialize());
 app.auth = auth;
-
+app.use('/', (req, res) => res.send({ message: 'Bem vindo a Books API!' }));
 booksRouter(app);
 usersRouter(app);
 authRouter(app);
