67dbcb5fe24e45dcbc66e4f7069544483404d902
João Manoel Neto <joaaomanooel@gmail.com>
2019-01-18 10:24:15 -0200

    clear app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const config = require('./config/config');
-const datasource = require('./config/datasource');
-const booksRouter = require('./app/routes/books');
-const usersRouter = require('./app/routes/users');
-const authRouter = require('./app/routes/auth');
 
 const app = express();
 app.use(cors());
@@ -15,13 +10,11 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.config = config;
-app.datasource = datasource(app);
 app.set('port', process.env.PORT || 5000);
+app.config = require('./config/config');
+app.datasource = require('./config/datasource')(app);
+require('./app/routes/index')(app);
 
-booksRouter(app);
-usersRouter(app);
-authRouter(app);
 app.use('/', (req, res) => res.send({ message: 'Bem vindo a Books API!' }));
 
 module.exports = app;
