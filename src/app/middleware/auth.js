3b22f34885361202d2e7987a2392a60e26d1ae6c
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-06 18:22:17 -0300

    clean auth middleware

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,4 +1,4 @@
-const HttpStatus = require('http-status');
+const { UNAUTHORIZED } = require('http-status');
 const jwt = require('jsonwebtoken');
 const config = require('../../config/config');
 
@@ -6,22 +6,19 @@ const config = require('../../config/config');
 module.exports = (req, res, next) => {
   const authHeaders = req.headers.authorization;
 
-  if (!authHeaders) return res.status(HttpStatus.UNAUTHORIZED).send({ error: 'No token provided' });
+  if (!authHeaders) return res.status(UNAUTHORIZED).send({ error: 'No token provided' });
 
   const parts = authHeaders.split(' ');
 
-  if (!parts.length === 2) return res.status(HttpStatus.UNAUTHORIZED).send({ error: 'Token error' });
+  if (!parts.length === 2) return res.status(UNAUTHORIZED).send({ error: 'Token error' });
 
   const [scheme, token] = parts;
+  const hasBearer = !/^Bearer$/i.test(scheme);
 
-  if (!/^Bearer$/i.test(scheme)) {
-    return res
-      .status(HttpStatus.UNAUTHORIZED)
-      .send({ error: 'Token malformated' });
-  }
+  if (hasBearer) return res.status(UNAUTHORIZED).send({ error: 'Token malformated' });
 
   jwt.verify(token, config.jwtSecret, (err, decoded) => {
-    if (err) return res.status(HttpStatus.UNAUTHORIZED).send({ error: 'Token invalid' });
+    if (err) return res.status(UNAUTHORIZED).send({ error: 'Token invalid' });
     req.userId = decoded.params.id;
 
     return next();
