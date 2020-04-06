2b433e1719221e4781af39265dfd9386a30db45f
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-06 18:23:00 -0300

    clean books controller

--- src/app/controllers/books.js
+++ src/app/controllers/books.js
@@ -1,13 +1,7 @@
-const httpStatus = require('http-status');
+const { OK, BAD_REQUEST, CREATED, UNPROCESSABLE_ENTITY, NO_CONTENT } = require('http-status');
 
-const defaultResponse = (data, statusCode = httpStatus.OK) => ({
-  data,
-  statusCode,
-});
-
-const errorResponse = (message, statusCode = httpStatus.BAD_REQUEST) => defaultResponse({
-  error: message,
-}, statusCode);
+const defaultResponse = (data, statusCode = OK) => ({ data, statusCode });
+const errorResponse = (error, statusCode = BAD_REQUEST) => defaultResponse({ error }, statusCode);
 
 class BooksController {
   constructor(Books) {
@@ -28,20 +22,20 @@ class BooksController {
 
   create(data) {
     return this.Books.create(data)
-      .then(result => defaultResponse(result, httpStatus.CREATED))
-      .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
+      .then(result => defaultResponse(result, CREATED))
+      .catch(error => errorResponse(error.message, UNPROCESSABLE_ENTITY));
   }
 
   update(data, params) {
     return this.Books.update(data, { where: params })
       .then(result => defaultResponse(result))
-      .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
+      .catch(error => errorResponse(error.message, UNPROCESSABLE_ENTITY));
   }
 
   delete(params) {
     return this.Books.destroy({ where: params })
-      .then(result => defaultResponse(result, httpStatus.NO_CONTENT))
-      .catch(error => errorResponse(error.message, httpStatus.UNPROCESSABLE_ENTITY));
+      .then(result => defaultResponse(result, NO_CONTENT))
+      .catch(error => errorResponse(error.message, UNPROCESSABLE_ENTITY));
   }
 }
 
