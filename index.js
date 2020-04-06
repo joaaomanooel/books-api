81ed4f9ed50d9e898d9c9649034ad1657a64100b
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-06 18:17:07 -0300

    update index.js to use renamed file

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const app = require('./src/app');
+/* eslint-disable no-console */
+const app = require('./src');
 
-app.listen(app.get('port'), () => {
-  console.log(`app is running on port ${app.get('port')}`);
-});
+app.listen(app.get('port'), () => console.log(`app is running on port ${app.get('port')}`));
