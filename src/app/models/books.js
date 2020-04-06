90e55bd8e644dda3d5cc385d60b6d57b8252ecee
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-06 18:21:37 -0300

    clean books models

--- src/app/models/books.js
+++ src/app/models/books.js
@@ -1,25 +1,5 @@
-module.exports = (sequelize, DataType) => {
-  const books = sequelize.define('Books', {
-    id: {
-      type: DataType.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataType.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    description: {
-      type: DataType.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-  });
-
-  return books;
-};
+module.exports = (sequelize, DataType) => sequelize.define('Books', {
+  id: { type: DataType.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataType.STRING, allowNull: false, validate: { notEmpty: true } },
+  description: { type: DataType.STRING, allowNull: false, validate: { notEmpty: true } },
+});
