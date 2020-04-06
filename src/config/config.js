9c4b16378185f0413c214eb4341971480d5ce685
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-06 18:19:50 -0300

    fix deprecated configs

--- src/config/config.js
+++ src/config/config.js
@@ -4,10 +4,9 @@ module.exports = {
   password: '',
   params: {
     dialect: 'sqlite',
+    operatorsAliases: 0,
     storage: `${process.env.NODE_ENV || 'prod'}_books.sqlite`,
-    define: {
-      undercored: true,
-    },
+    define: { undercored: true },
   },
   jwtSecret: '63028de2d2e2e2cc97748dbe0c32b3e5',
 };
