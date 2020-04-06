168bf56eb737bc3512e78c879fe7e95fd85635b3
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-06 18:20:53 -0300

    clean user models

--- src/app/models/users.js
+++ src/app/models/users.js
@@ -1,48 +1,20 @@
 const bcrypt = require('bcrypt');
 
-module.exports = (sequelize, DataType) => {
-  const Users = sequelize.define('Users', {
-    id: {
-      type: DataType.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataType.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    email: {
-      type: DataType.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-    password: {
-      type: DataType.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-  }, {
-    hooks: {
-      beforeUpdate: (user) => {
-        const salt = bcrypt.genSaltSync();
-        user.set('password', bcrypt.hashSync(user.password, salt));
-      },
-      beforeCreate: (user) => {
-        const salt = bcrypt.genSaltSync();
-        user.set('password', bcrypt.hashSync(user.password, salt));
-      },
-    },
-    classMethods: {
-      isPassword: (encodedPassword, password) => bcrypt.compareSync(password, encodedPassword),
-    },
-  });
-
-  return Users;
+const beforeUpdate = (user) => {
+  const salt = bcrypt.genSaltSync();
+  user.set('password', bcrypt.hashSync(user.password, salt));
 };
+
+const beforeCreate = (user) => {
+  const salt = bcrypt.genSaltSync();
+  user.set('password', bcrypt.hashSync(user.password, salt));
+};
+
+const isPassword = (encodedPassword, password) => bcrypt.compareSync(password, encodedPassword);
+
+module.exports = (sequelize, DataType) => sequelize.define('Users', {
+  id: { type: DataType.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataType.STRING, allowNull: false, validate: { notEmpty: true } },
+  email: { type: DataType.STRING, allowNull: false, unique: true, validate: { notEmpty: true } },
+  password: { type: DataType.STRING, allowNull: false, validate: { notEmpty: true } },
+}, { hooks: { beforeUpdate, beforeCreate }, classMethods: { isPassword } });
