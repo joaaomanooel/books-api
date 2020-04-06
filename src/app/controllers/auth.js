8b1ecd14b4ad086b008560e1de5e21dea0fec638
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-06 18:22:45 -0300

    clean auth controller

--- src/app/controllers/auth.js
+++ src/app/controllers/auth.js
@@ -1,35 +1,23 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const HttpStatus = require('http-status');
-const authConfig = require('../../config/config');
+const { BAD_REQUEST } = require('http-status');
+const { jwtSecret } = require('../../config/config');
 
-const generateToken = (params = {}) => {
-  return jwt.sign(
-    { params },
-    authConfig.jwtSecret,
-    { expiresIn: 86400 }
-  );
-}
+const generateToken = (params = {}) => jwt.sign({ params }, jwtSecret, { expiresIn: 86400 });
 
 const authenticate = async (req, res, app) => {
   const { Users } = app.datasource.models;
   const { email, password } = req.body;
-  if (!email || !password) return res.status(HttpStatus.BAD_REQUEST).send({ error: 'User not found.' });
+  if (!email || !password) return res.status(BAD_REQUEST).send({ error: 'User not found.' });
   const user = await Users.findOne({ where: { email } });
-  if (!user) return res.status(HttpStatus.BAD_REQUEST).send({ error: 'User not found.' });
+  if (!user) return res.status(BAD_REQUEST).send({ error: 'User not found.' });
   if (!await bcrypt.compare(password, user.password)) {
-    return res.status(HttpStatus.BAD_REQUEST).send({ error: 'Invalid password.' });
+    return res.status(BAD_REQUEST).send({ error: 'Invalid password.' });
   }
 
   user.password = undefined;
 
-  return res.send({
-    user,
-    token: generateToken({ id: user._id }),
-  });
+  return res.send({ user, token: generateToken({ id: user._id }) });
 };
 
-module.exports = {
-  authenticate,
-  generateToken
-}
+module.exports = { authenticate, generateToken };
