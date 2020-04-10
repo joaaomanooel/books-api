a03dd5a4c12f74003493e9c8b18bea407b9c5230
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-10 20:46:10 -0300

    add redemeio config

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const readme = require('readmeio');
 
 const app = express();
+if (process.env.NODE_ENV !== 'test') {
+  app.use(readme.metrics(process.env.READEME_TOKEN, req => ({
+    id: req.userId,
+    label: req.userName,
+    email: req.userEmail,
+  }), { development: process.env.NODE_ENV === 'development' }));
+}
 app.use(cors());
 app.use(morgan('dev'));
 app.use(helmet());
@@ -15,6 +23,8 @@ app.config = require('./config/config');
 app.datasource = require('./config/datasource')(app);
 require('./app/routes/index')(app);
 
+
 app.use('/', (_req, res) => res.send({ message: 'Bem vindo a Books API!' }));
+
 
 module.exports = app;
