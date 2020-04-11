9d3a5960817567369f5d93fc30a29009cf0e65f0
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-10 22:02:59 -0300

    move readmeio config

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 /* eslint-disable no-console */
 require('dotenv').config();
+const readme = require('readmeio');
 const app = require('./src');
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(readme.metrics(process.env.READEME_TOKEN, req => ({
+    id: req.userId,
+    label: req.userName,
+    email: req.userEmail,
+  }), { development: process.env.NODE_ENV === 'development' }));
+}
 
 app.listen(app.get('port'), () => console.log(`app is running on port ${app.get('port')}`));
--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const readme = require('readmeio');
 
 const app = express();
-if (process.env.NODE_ENV !== 'test') {
-  app.use(readme.metrics(process.env.READEME_TOKEN, req => ({
-    id: req.userId,
-    label: req.userName,
-    email: req.userEmail,
-  }), { development: process.env.NODE_ENV === 'development' }));
-}
 app.use(cors());
 app.use(morgan('dev'));
 app.use(helmet());
